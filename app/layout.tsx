016fa889ae882352a1718a0a8ffbbb62dbac67ef
KemiZHANG <links358p@gmail.com>
2026-05-31 05:15:51 +0800

    fix: declare utf8 document encoding

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="zh-CN">
+      <head>
+        <meta charSet="utf-8" />
+      </head>
       <body>{children}</body>
     </html>
   );
